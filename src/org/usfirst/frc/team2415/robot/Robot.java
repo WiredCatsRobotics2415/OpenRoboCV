44f8e894452e459b8985d4985552c53ae269eee8
Arxos0012 <jabaribooker@westminster.net>
2016-02-29 17:14:52 -0500

    Now has networking

--- src/org/usfirst/frc/team2415/robot/Robot.java
+++ src/org/usfirst/frc/team2415/robot/Robot.java
@@ -1,12 +1,13 @@
 
 package org.usfirst.frc.team2415.robot;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import com.ni.vision.NIVision;
 
@@ -15,34 +16,20 @@ import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
 public class Robot extends SampleRobot {
-	static{
-		try{
-			System.load("/home/lvuser/lib_OpenCV/java/libopencv_java2410.so");
-		}catch(UnsatisfiedLinkError e){
-			e.printStackTrace();
-		}
-	}
-	
-	final int HUE_L = 0, SAT_L = 217, VIS_L = 204,
-			  HUE_H = 20, SAT_H = 255, VIS_H = 255;
-	
-	final Scalar low = new Scalar(HUE_L, SAT_L, VIS_L);
-	final Scalar high = new Scalar(HUE_H, SAT_H, VIS_H);
-	
-	Mat src, bin, morph;
-	Mat struct;
 	
 	USBCamera niCam;
 	NIVision.Image frame;
 	NIVision.RawData colorTable;
-    
+	
+	final int PORT = 2415;
+	
+	ServerSocket imgServerSocket;
+	OutputStream out;
+	
     public void robotInit() {
-    	src = bin = morph = new Mat();
-    	struct = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5));
-    	
     	
     	niCam = new USBCamera("cam0");
-    	frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_HSL, 0);
+    	frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
     	colorTable = new NIVision.RawData();
     	
     	niCam.openCamera();
@@ -52,6 +39,13 @@ public class Robot extends SampleRobot {
 		niCam.setExposureHoldCurrent();
 		niCam.setWhiteBalanceManual(USBCamera.WhiteBalance.kFixedFlourescent2);
 		niCam.setWhiteBalanceHoldCurrent();
+		
+		try {
+			imgServerSocket = new ServerSocket(PORT);
+			imgServerSocket.setSoTimeout(10000);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
     
     
@@ -66,21 +60,37 @@ public class Robot extends SampleRobot {
     public void autonomous() {
     	long start = System.currentTimeMillis();
     	int secs = 3, lastSec = -1;
+    	
+    	System.out.print("Taking capture in ");
     	while((System.currentTimeMillis() - start)/1000.0 <= secs){
-    		if((System.currentTimeMillis() - start)/1000 != lastSec)
-				System.out.println((secs - (System.currentTimeMillis() - start)/1000) + "!");
+    		if((System.currentTimeMillis() - start)/1000 != lastSec){
+    			System.out.println((secs - ((System.currentTimeMillis() - start)/1000)) + "!");
     			lastSec = (int)(System.currentTimeMillis() - start)/1000;
+    		}
     	}
+    	
     	niCam.getImage(frame);
     	NIVision.imaqWriteJPEGFile(frame, "/home/lvuser/img.jpg", 2000, colorTable);
     	
-    	src = Highgui.imread("/home/lvuser/img.jpg");
-    	Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2HSV);
-    	Core.inRange(src, low, high, bin);
-    	Highgui.imwrite("/home/lvuser/bin.jpg", bin);
-    	
-    	Imgproc.dilate(bin, morph, struct);
-    	Highgui.imwrite("/home/lvuser/morph.jpg", morph);
+    	try {
+			Socket server = imgServerSocket.accept();
+			out = server.getOutputStream();
+			File load = new File("/home/lvuser/img.jpg");
+			byte[] buffer = new byte[(int)load.length()];
+			
+			int count;
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(load));
+			
+			while((count = in.read(buffer)) > 0){
+				  out.write(buffer, 0, count);
+				  out.flush();
+			}
+			
+			server.close();
+			System.out.println("Image sent!");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
     }
     
     public void operatorControl() {
